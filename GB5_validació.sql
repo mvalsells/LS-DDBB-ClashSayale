481c5fa79fb8d4ae196392aaa30c0843a1ac506f
alberttomas87 <alberttomasroman@gmail.com>
2022-01-12 11:59:16 +0100

    Organitazció per a la validació

--- GB5_validació.sql
+++ GB5_validació.sql
@@ -1,2 +1,18 @@
 -- BBDD GB5 - Marc Valsells, Marc Geremias, Irina Aynes i Albert Tomas
 -- Validació
+
+-- Validació entorn a Jugadors
+
+-- Validació entorn a Cartes
+
+-- Validació entorn a Clans
+
+-- Validació entorn a Batalles
+
+-- Validació entorn a Arenes
+
+-- Validació entorn a Missions
+
+-- Validació entorn a Assoliments
+
+-- Validació entorn a Botiga
