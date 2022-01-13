d1769e1dceac16c44ac6a2970cb60c740315888f
irinaaynes <96120010+irinaaynes@users.noreply.github.com>
2022-01-13 17:04:50 +0100

    Validacions Botiga i assoliments(1/2)

--- GB5_validació.sql
+++ GB5_validació.sql
@@ -79,12 +79,61 @@ SELECT tag_jugador,titol,descripcio,id_arena,data,recompensa_gemmes
 FROM assoliment as a, aconsegueix as ac
 WHERE a.id_assoliment = ac.id_assoliment;
 
+--Jugador en una arena determinada quantes gemmes(assoliment) te
+SELECT DISTINCT j.tag_jugador as ID_Jugador,j.nom as Nom_Jugador,a.id_arena as ID_Arena,
+a2.recompensa_gemmes as Assoliment_Gemmes
+FROM jugador as j JOIN aconsegueix a on j.tag_jugador = a.tag_jugador
+JOIN assoliment a2 on a.id_assoliment = a2.id_assoliment;
+
 --falta atribut arena que noe sta posat a cap taula
 --SELECT tag_jugador, i.titol, id_arena,data,imatge
 --FROM insignia as i, guanya as g, arena as a
 --WHERE i.
 
 -- Validació entorn a Botiga
+--(Quants articles han comprat els 10 primers jugadors per ordre abc)
+SELECT j.tag_jugador as Id_Jugador, j.nom as Nom, COUNT(c.id_article) as Articles
+FROM compren AS c JOIN targeta_credit tc on c.num_targeta = tc.numero
+JOIN jugador j on c.tag_jugador = j.tag_jugador
+JOIN article a on c.id_article = a.id_article
+GROUP BY j.tag_jugador,j.nom
+ORDER BY j.nom asc
+LIMIT 10;
+
+--(Un jugador compra un article que es vegi la quantitat el preu i el seu nom, descompte)
+SELECT DISTINCT j.tag_jugador as ID_Jugador, j.nom as Nom, t.numero, a.nom as Nom_article,
+a.preu as Preu_article, a.quantitat as Quants, c.descompte as Descompte,c.data_ as Data
+FROM jugador as j JOIN targeta_credit as t on j.targeta_credit = t.numero
+JOIN compren c on j.tag_jugador = c.tag_jugador and t.numero = c.num_targeta
+JOIN article a on c.id_article = a.id_article;
+
+--(Jugador compra paquet que conte)
+SELECT j.tag_jugador as ID_Jugador,j.nom as NOM, co.nom_cofre as Paquet_Cofre,a.preu as Preu_article,
+co.raresa as Raresa, co.temps as Temps_Desbloqueig, co.quantitat_cartes as Num_Cartes
+FROM article as a join cofre co on a.id_article = co.id_cofre
+JOIN compren c on a.id_article = c.id_article
+JOIN jugador j on c.tag_jugador = j.tag_jugador;
+
+--Paquet bundle que conte
+SELECT  b.or_ as Bundle_Or, b.gemmes as Bundle_Gemmes
+FROM  bundle AS b;
+
+--Paquet emoticones
+SELECT j.tag_jugador as ID_Jugador,j.nom, a.nom as Nom_Article, e.nom_imatge
+as nom_imatge, e.direccio_imatge as Direccio_imatge
+FROM compren as c JOIN article a on c.id_article = a.id_article
+JOIN jugador j on c.tag_jugador = j.tag_jugador
+JOIN emoticones e on a.id_article = e.id_emoticones
+LIMIT 20;
+
+--Paquet arena
+SELECT DISTINCT j.tag_jugador as ID_Jugador,j.nom as Nom_Jugador, a.preu as Preu_Article,
+ap.id_pack AS Paquet_Arena_Id,apa.or_ as Paquet_Arena_Or
+FROM jugador as j JOIN compren c on j.tag_jugador = c.tag_jugador
+JOIN article as a  on a.id_article = c.id_article
+JOIN arena_pack as ap on ap.id_pack = a.id_article
+JOIN arena_pack_arena as apa on apa.id_arena_pack = ap.id_pack;
 
 
---arena pack?
+
+
