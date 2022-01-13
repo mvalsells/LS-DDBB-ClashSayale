156e8800e9bae23a829f3ed3c5ab2aef95b524fb
Marc Valsells <marc@valsells.me>
2022-01-13 09:33:13 +0100

    Selects missio

--- GB5_validació.sql
+++ GB5_validació.sql
@@ -27,6 +27,37 @@ FROM lluiten;
 -- Validació entorn a Arenes
 
 -- Validació entorn a Missions
+SELECT j.tag_jugador AS tag_jugador, j.nom AS nom_jugador, count(c.id_missio) AS num_missions
+FROM completen AS c
+JOIN jugador AS j on c.tag_jugador = j.tag_jugador
+GROUP BY j.tag_jugador
+ORDER BY num_missions DESC;
+
+SELECT j.tag_jugador AS tag_jugador, j.nom AS nom_jugador, SUM(c.or_) AS or_guanyat, SUM(c.experiencia) AS expereincia_guanyada
+FROM completen AS c
+JOIN jugador AS j on c.tag_jugador = j.tag_jugador
+GROUP BY j.tag_jugador
+ORDER BY or_guanyat DESC, expereincia_guanyada DESC;
+
+SELECT m.id_missio, m.titol AS titol_missio, COUNT(c.id_arena) AS num_arenes
+FROM missio AS m
+JOIN completen AS c on m.id_missio = c.id_missio
+GROUP BY m.id_missio;
+
+SELECT m.titol AS Missio, (SELECT m2.titol FROM missio AS m2 WHERE m2.id_missio = d.id_missio2) AS depen
+FROM missio AS m
+JOIN depen AS d on m.id_missio = d.id_missio1;
+
+SELECT j.tag_jugador, j.nom, a.titol AS titol_arena, SUM(c.experiencia) AS experiencia
+FROM arena AS a
+JOIN completen AS c on a.id_arena = c.id_arena
+JOIN jugador AS j on c.tag_jugador = j.tag_jugador
+GROUP BY a.id_arena, j.tag_jugador
+ORDER BY experiencia DESC
+LIMIT 10;
+
+-- SELECT DISTINCT count(id_missio1)
+-- FROM depen
 
 -- Validació entorn a Assoliments
 
