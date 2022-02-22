e39c047d5ec460b11908fa3c78815d7bf7f5e6b9
alberttomas87 <alberttomasroman@gmail.com>
2022-02-22 13:08:06 +0100

    Preparació dels enunciats

--- Fase3/set4.sql
+++ Fase3/set4.sql
@@ -1,2 +1,49 @@
--- BBDD GB5 - Marc Valsells, Marc Geremias, Irina Aynes i Albert Tomas
--- Set 4 - Tingueu valor. Encara tenim el nostre clan. Sempre hi ha esperança.
+-- BBDD GB5 - Marc Valsells, Marc Geremias, Irina Aynés i Albert Tomàs
+
+-- Set 4 - M'agrada la competició. M'agraden els reptes...
+
+
+-- CONSULTES:
+
+/* 1. Enumera el nom, els trofeus mínims, els trofeus màxims de les arenes que el seu títol
+comença per "A" i tenen un paquet d’arena amb or superior a 8000. */
+
+
+
+/* 2. Llista de nom, data d'inici, data de finalització de les temporades i, de les batalles
+d'aquestes temporades, el nom del jugador guanyador si el jugador té més victòries que
+derrotes i la seva experiència és més gran de 200.000. */
+
+
+
+/* 3. Llistar la puntuació total dels jugadors guanyadors de batalles de cada temporada. Filtrar
+la sortida per considerar només les temporades que han començat i acabat el 2019. */
+
+
+/* 4. Enumerar els noms de les arenes en què els jugadors veterans (experiència superior a
+170.000) van obtenir insígnies després del "25-10-2021". Ordenar el resultat segons el
+nom de l’arena en ordre ascendent. */
+
+
+
+/* 5. Enumerar el nom de la insígnia, els noms de les cartes i el dany de les cartes dels
+jugadors amb una experiència superior a 290.000 i obtingudes en arenes el nom de les
+quals comença per "A" o quan la insígnia no té imatge. Així, considera només els
+jugadors que tenen una carta el nom de la qual comença per "Lava". */
+
+
+
+/* 6. Donar el nom de les missions que donen recompenses a totes les arenes el títol de les
+quals comença per "t" o acaba per "a". Ordena el resultat pel nom de la missió. */
+
+
+
+/* 7. Donar el nom de les arenes amb jugadors que al novembre o desembre de 2021 van
+obtenir insígnies si el nom de l’arena conté la paraula "Lliga", i les arenes tenen jugadors
+que al 2021 van obtenir èxits el nom dels quals conté la paraula "Friend". */
+
+
+
+/* 8. Retorna el nom de les cartes que pertanyen a jugadors que van completar missions el
+nom de les quals inclou la paraula "Armer" i l'or de la missió és més gran que l'or mitjà
+recompensat en totes les missions de les arenes */
