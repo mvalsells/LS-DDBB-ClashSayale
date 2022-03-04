e058da238724db74024da3057f28dd2cc584c7d9
Marc Valsells <marc@valsells.me>
2022-03-04 08:28:12 +0100

    Afegir enunciats preguntes creuades

--- Fase3/set5.sql
+++ Fase3/set5.sql
@@ -1,2 +1,60 @@
 -- BBDD GB5 - Marc Valsells, Marc Geremias, Irina Aynes i Albert Tomas
 -- Set 5 - Preguntes creuades
+
+-- 1. Mostrar el nombre de jugadors que té cada clan, però només considerant els jugadors
+-- amb nom de rol que contingui el text "elder". Restringir la sortida per als 5 primers clans
+-- amb més jugadors.
+
+
+-- 2. Mostrar el nom dels jugadors, el text dels missatges i la data dels missatges enviats pels
+-- jugadors que tenen la carta Skeleton Army i han comprat articles abans del 01-01-2019.
+
+
+-- 3. Llistar els 10 primers jugadors amb experiència superior a 100.000 que han creat més
+-- piles i han guanyat batalles a la temporada T7.
+
+
+-- 4. Enumera els articles que han estat comprats més vegades i el seu cost total.
+
+
+-- 5. Mostrar la identificació de les batalles, la durada, la data d'inici i la data de finalització
+-- dels clans que la seva descripció no contingui el text "Chuck Norris". Considera només
+-- les batalles amb una durada inferior a la durada mitjana de totes les batalles.
+
+
+-- 6. Enumerar el nom i l'experiència dels jugadors que pertanyen a un clan que té una
+-- tecnologia el nom del qual conté la paraula "Militar" i aquests jugadors havien comprat
+-- el 2021 més de 5 articles.
+
+
+-- 7. Indiqueu el nom dels jugadors que tenen totes les cartes amb el major valor de dany.
+
+
+-- 8. Retorna el nom de les cartes i el dany que pertanyen a les piles el nom de les quals
+-- conté la paraula "Madrid" i van ser creats per jugadors amb experiència superior a
+-- 150.000. Considereu només les cartes amb dany superior a 200 i els jugadors que van
+-- aconseguir un èxit en el 2021. Enumera el resultat des dels valors més alts del nom de
+-- la carta fins als valors més baixos del nom de la carta.
+
+
+-- 9. Enumerar el nom, l’experiència i el nombre de trofeus dels jugadors que no han comprat
+-- res. Així, el nom, l'experiència i el número de trofeus dels jugadors que no han enviat
+-- cap missatge. Ordenar la sortida de menor a més valor en el nom del jugador.
+
+
+-- 10.Llistar les cartes comunes que no estan incloses en cap pila i que pertanyen a jugadors
+-- amb experiència superior a 200.000. Ordena la sortida amb el nom de la carta.
+
+
+-- 11.Llistar el nom dels jugadors que han sol·licitat amics, però no han estat sol·licitats com
+-- a amics.
+
+
+-- 12.Enumerar el nom dels jugadors i el nombre d'articles comprats que tenen un cost
+-- superior al cost mitjà de tots els articles. Ordenar el resultat de menor a major valor del
+-- nombre de comandes.
+
+
+-- 13.Poseu a zero els valors d'or i gemmes als jugadors que no han enviat cap missatge o
+-- que han enviat el mateix nombre de missatges que el jugador que més missatges ha
+-- enviat.
